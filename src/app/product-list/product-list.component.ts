c6e1332996cff98fcac879aa0940bcb4f7853a2e
CMimi23 <101603877+CMimi23@users.noreply.github.com>
2023-10-23 11:32:53 +0300

    Product list + product details

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,6 +13,10 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared!');
   }
+
+  onNotifyEmitter() {
+    window.alert("You will be notified whent this product is going to be on a sale")
+}
 }
 
 
